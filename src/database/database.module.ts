42967fa78391dedf732c6c46baef01c3bc904389
TagAls <alsbglgs@hotmail.com>
2024-01-08 15:19:02 -0300

    Configuração de modulo para comunicação com banco de dados

---
+++ src/database/database.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Produto } from 'src/produto/entities/produto.entity';
+import { Categoria } from 'src/categoria/entities/categoria.entity';
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('DATABASE_URL'),
+        entities: [Produto, Categoria],
+        synchronize: true,
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
