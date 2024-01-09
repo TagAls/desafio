dd16fad35ecc74457a692b7d7a0c4b511432207d
TagAls <alsbglgs@hotmail.com>
2024-01-09 08:37:39 -0300

    Inclusão de use case para realizar calculo de parcelas.

---
+++ src/produto/use-cases/calcula-parcela-produto.use-case.ts
@@ -0,0 +1,32 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { CalculoParcelaProdutoDTO } from '../dto/calculo-parcela-produto.dto';
+import { Produto } from '../entities/produto.entity';
+import { Categoria } from 'src/categoria/entities/categoria.entity';
+import { Repository } from 'typeorm';
+
+export class CalculoParcelaUseCase {
+  constructor(
+    @InjectRepository(Produto) private produtoRepo: Repository<Produto>,
+    @InjectRepository(Categoria) private categoriaRepo: Repository<Categoria>,
+  ) {}
+  async execute(input: CalculoParcelaProdutoDTO) {
+    let valorParcela = 0;
+    if (input.parcelas > 0) {
+      const nome = input.nome;
+      const produto = this.produtoRepo.findOne({ where: { nome } });
+      await produto.then(async (p) => {
+        const id = p.idCategoria;
+        const categoria = this.categoriaRepo.findOne({ where: { id } });
+        await categoria.then(async (c) => {
+          const porcentagem = c.juros / 100;
+          valorParcela =
+            (p.valor * c.juros) /
+            (1 - Math.pow(1 * porcentagem, input.parcelas));
+        });
+      });
+      return valorParcela.toFixed(2);
+    } else {
+      return 'Não foi possivel realizar o calculo pois o número de parcelas deve ser maior que 0';
+    }
+  }
+}
