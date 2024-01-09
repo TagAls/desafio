86a1219c67604ddd9c70fe66e7f71ba18fa38b9a
TagAls <alsbglgs@hotmail.com>
2024-01-09 14:28:29 -0300

    Ajuste de arquivos de testes para funcionalidade de todos os processos de CRUD.

--- test/produto-categoria-testes.e2e.spec.ts
+++ test/produto-categoria-testes.e2e.spec.ts
@@ -3,8 +3,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import request from 'supertest';
 import { AppModule } from '../src/app.module';
 
-describe('ProdutoUseCaseController (e2e)', () => {
+describe('ProdutoUseCaseController e CategoriaUseCaseController (e2e)', () => {
   let idProduto = '';
+  let idCategoria = '';
   let app: INestApplication;
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -14,12 +15,56 @@ describe('ProdutoUseCaseController (e2e)', () => {
     await app.init();
   });
 
+  it('POST /categorias', async () => {
+    const requestData = {
+      nome: 'teste',
+      juros: 5,
+    };
+    const response = await request(app.getHttpServer())
+      .post('/categorias')
+      .send(requestData)
+      .expect(HttpStatus.CREATED);
+    idCategoria = response.body.id;
+  });
+
+  it('GET /categorias', async () => {
+    const response = await request(app.getHttpServer())
+      .get('/categorias')
+      .expect(HttpStatus.OK);
+
+    expect(response.body[0]).toHaveProperty('id');
+    expect(response.body[0]).toHaveProperty('nome');
+    expect(response.body[0]).toHaveProperty('juros');
+  });
+
+  it('GET /categorias/idCategoria', async () => {
+    const response = await request(app.getHttpServer())
+      .get(`/categorias/${idCategoria}`)
+      .expect(HttpStatus.OK);
+
+    expect(response.body).toHaveProperty('id');
+    expect(response.body).toHaveProperty('nome');
+    expect(response.body).toHaveProperty('juros');
+  });
+
+  it('PATCH /categorias/idCategoria', async () => {
+    const response = await request(app.getHttpServer()).get(
+      '/categorias/' + idCategoria,
+    );
+    response.body.juros = 2.5;
+    const updateReponse = await request(app.getHttpServer())
+      .patch(`/categorias/${idCategoria}`)
+      .send(response.body)
+      .expect(HttpStatus.OK);
+    expect(updateReponse.body).toEqual(response.body);
+  });
+
   it('POST /produtos', async () => {
     const requestData = {
       nome: 'roda',
       descricao: 'roda de carro',
       valor: 150,
-      idCategoria: 'ac5d405e-afe0-4e5b-838d-6d6cecb0ebf3',
+      idCategoria: idCategoria,
     };
     const response = await request(app.getHttpServer())
       .post('/produtos')
@@ -70,14 +115,20 @@ describe('ProdutoUseCaseController (e2e)', () => {
     response.body.descricao = 'roda de bicicleta';
     const updateReponse = await request(app.getHttpServer())
       .patch(`/produtos/${idProduto}`)
-      .send(response.body);
-    expect(HttpStatus.OK);
+      .send(response.body)
+      .expect(HttpStatus.OK);
     expect(updateReponse.body).toEqual(response.body);
   });
 
   it('DELETE /produtos', async () => {
     await request(app.getHttpServer())
       .delete('/produtos/' + idProduto)
+      .expect(HttpStatus.OK);
+  });
+
+  it('DELETE /categorias', async () => {
+    await request(app.getHttpServer())
+      .delete('/categorias/' + idCategoria)
       .expect(HttpStatus.OK);
   });
 });
